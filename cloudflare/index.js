1838cb6bb2372c820145bb7f0ead823d2ea38787
Inian <inian1234@gmail.com>
2022-02-01 17:08:12 +0800

    try cloudflare worker with pg

--- cloudflare/index.js
+++ cloudflare/index.js
@@ -1,3 +1,14 @@
+// does not work
+// cos it requires the dns module which is not present in the cloudflare environment
+
+const { Client } = require("pg");
+const client = new Client();
+
+async function generateResponse() {
+  await client.connect();
+  return new Response("Hello miniflare");
+}
+
 addEventListener("fetch", (event) => {
-  event.respondWith(new Response("Hello Miniflare!"));
+  event.respondWith(generateResponse());
 });
